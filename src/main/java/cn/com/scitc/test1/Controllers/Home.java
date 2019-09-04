40354b9361cc1031403a7c2a44a0c3380c779bd1
zql <742715157@qq.com>
2019-09-04 09:15:22 +0800

    second

---
+++ src/main/java/cn/com/scitc/test1/Controllers/Home.java
@@ -0,0 +1,4 @@
+package cn.com.scitc.test1.Controllers;
+
+public class Home {
+}
--- src/main/java/cn/com/scitc/test1/Controllers/HomeController.java
+++ src/main/java/cn/com/scitc/test1/Controllers/HomeController.java
@@ -1,12 +1,6 @@
 package cn.com.scitc.test1.Controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping(value = "/")
 public class HomeController {
-    @RequestMapping(value = "/")
     public String page1(){
         return "home";
     }
